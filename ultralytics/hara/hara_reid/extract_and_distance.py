8049d7902cef8d15e1a629341a161874f5a07195
Taiping Liu <tliu25@GMU.EDU>
2026-05-25 23:05:25 -0400

    ReID matrix

--- ultralytics/hara/hara_reid/extract_and_distance.py
+++ ultralytics/hara/hara_reid/extract_and_distance.py
@@ -52,6 +52,47 @@ def main():
     print("cosine distance around 0.45 -> probably different")
     print("cosine distance around 0.80 -> very different")
 
+def main_distance():
+    output = Path("./outputs")
+    model_path = r"C:\Users\tliu25\workspace\ultralytics\ultralytics\hara\hara_reid\outputs\best_osnet.pth"
+    extractor = ChickenFeatureExtractor(model_path)
+
+    track_paths = list_images(r"C:\Users\tliu25\workspace\ultralytics\ultralytics\hara\hara_reid\hara_distance_test\track")
+    detection_paths = list_images(r"C:\Users\tliu25\workspace\ultralytics\ultralytics\hara\hara_reid\hara_distance_test\detection")
+    track_features = extractor.extract(track_paths, batch_size=32)
+    detection_features = extractor.extract(detection_paths, batch_size=32)
+
+    # This is the final result needed by DeepSORT.
+    cost_matrix = cosine_distances(track_features, detection_features)
+
+    df = pd.DataFrame(
+        cost_matrix,
+        index=[Path(p).name for p in track_paths],
+        columns=[Path(p).name for p in detection_paths],
+    )
+    df.to_csv(output / "cost_matrix.csv")
+
+    np.save(output / "track_features.npy", track_features)
+    np.save(output / "detection_features.npy", detection_features)
+
+    print("cost_matrix shape:", cost_matrix.shape)
+    print(df.round(4))
+    print(f"\nSaved to: {output / 'cost_matrix.csv'}")
+    print("\nInterpretation example:")
+    print("cosine distance around 0.08 -> very similar")
+    print("cosine distance around 0.45 -> probably different")
+    print("cosine distance around 0.80 -> very different")
+
+def main_feature():
+    model_path = r"C:\Users\tliu25\workspace\ultralytics\ultralytics\hara\hara_reid\outputs\best_osnet.pth"
+    extractor = ChickenFeatureExtractor(model_path)
+
+    track_paths = list_images(r"C:\Users\tliu25\workspace\ultralytics\ultralytics\hara\hara_reid\hara_distance_test\track")
+    track_features = extractor.extract(track_paths, batch_size=32)
+    print("---")
+    print(track_features.shape)
+    print(track_features)
+
 
 if __name__ == "__main__":
-    main()
+    main_feature()
